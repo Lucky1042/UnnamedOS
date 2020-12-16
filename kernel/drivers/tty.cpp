1c483c84e479bed45613da4a25a2a355b525319b
Lucky1042 <>
2020-12-15 22:23:34 -0500

    Made newlines work properly

--- kernel/drivers/tty.cpp
+++ kernel/drivers/tty.cpp
@@ -40,6 +40,12 @@ void terminal_putentryat(unsigned char c, uint8_t color, size_t x, size_t y) {
 
 void terminal_putchar(char c) {
 	unsigned char uc = c;
+	// Checks if character is a newline
+	if (uc == '\n') {
+		terminal_column = 0;
+		++terminal_row;
+		return;
+	}
 	terminal_putentryat(uc, terminal_color, terminal_column, terminal_row);
 	if (++terminal_column == VGA_WIDTH) {
 		terminal_column = 0;
